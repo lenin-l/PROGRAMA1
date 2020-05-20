c9ceb9e7694d68a3f8ef830344edf34ff44d957d
lenin-l <lenincarrillo1e@gmail.com>
2020-05-19 22:24:18 -0500

    Código Inicial

---
+++ src/programa_básico/Programa_básico.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programa_básico;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Programa_básico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo");
+
+        System.err.print("BIENVENIDOS TODOS");
+        //YO
+        System.out.println("COMO LES VA");
+    }
+
+}
